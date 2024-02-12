83ede4779d8b6c42f6c12ec7d2ae052c3d080466
RMSCA <howell.rmsca@outlook.com>
2024-02-12 15:08:58 -0500

    Initial commit

---
+++ src/main/java/com/laelioa/cbmod/ComfortBox.java
@@ -0,0 +1,19 @@
+package com.laelioa.cbmod;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = Reference.MODID, name = Reference.NAME, version = Reference.VERSION)
+public class ComfortBox {
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {}
+
+    @EventHandler
+    public void init(FMLInitializationEvent event) {}
+
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event) {}
+}
---
+++ src/main/java/com/laelioa/cbmod/Reference.java
@@ -0,0 +1,7 @@
+package com.laelioa.cbmod;
+
+public class Reference {
+    public static final String MODID = "cbmod";
+    public static final String NAME = "Comfortbox-Remake";
+    public static final String VERSION = "1.12.2-0.1.0";
+}
