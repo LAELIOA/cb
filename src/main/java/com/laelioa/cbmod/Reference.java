c493f09e49e6be98cf757f50d315e77cc02655b3
RMSCA <howell.rmsca@outlook.com>
2024-03-12 18:52:45 -0700

    Bump version

--- src/main/java/com/laelioa/cbmod/Reference.java
+++ src/main/java/com/laelioa/cbmod/Reference.java
@@ -3,7 +3,7 @@ package com.laelioa.cbmod;
 public class Reference {
     public static final String MODID = "cbmod";
     public static final String NAME = "Comfortbox-Remake";
-    public static final String VERSION = "1.12.2-0.1.0-beta";
+    public static final String VERSION = "1.12.2-0.1.0";
     public static final String CLIENT_PROXY = "com.laelioa.cbmod.proxy.ClientProxy";
     public static final String COMMON_PROXY = "com.laelioa.cbmod.proxy.CommonProxy";
 }
